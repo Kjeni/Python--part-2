3ede60d8ec665f95518885ad0dfa2b38e6e6db52
Kjeni <kacper.blazejewski88@gmail.com>
2023-04-27 16:04:02 +0200

    zad

---
+++ 3. Operatory logiczne/zad3.py
@@ -0,0 +1,10 @@
+wiek = int(input("Ile masz lat: "))
+a2 = True
+    if int(input("Czy masz prawo jazdy kat A2(t/n): ")):
+        in ("t","ta","tak","T")
+    else:
+         False
+
+if wiek > 24:
+    print("Możesz przystąpic do egzaminu na kat A")
+else:
---
+++ 4. Funkcje/zad.py
@@ -0,0 +1,42 @@
+def dodawanie():
+    liczba1 = int(input("Podaj pierwszą liczbę: "))
+    liczba2 = int(input("Podaj drugą liczbę: "))
+    wynik = liczba1+liczba2
+    return wynik
+def odejmowanie():
+    liczba1 = int(input("Podaj pierwszą liczbę: "))
+    liczba2 = int(input("Podaj drugą liczbę: "))
+    wynik = liczba1-liczba2
+    return wynik
+def mnożenie():
+    liczba1 = int(input("Podaj pierwszą liczbę: "))
+    liczba2 = int(input("Podaj drugą liczbę: "))
+    wynik = liczba1*liczba2
+    return wynik
+def dzielenie():
+    liczba1 = int(input("Podaj pierwszą liczbę: "))
+    liczba2 = int(input("Podaj drugą liczbę: "))
+    wynik = liczba1/liczba2
+    return wynik
+def menu():
+    print("[1] Dodawanie")
+    print("[2] Odejmowanie")
+    print("[3] Mnożenie")
+    print("[4] Dzielenie")
+
+menu()
+option = int(input("Co chcesz zrobić[1-4]: "))
+while option != 0:
+    if option ==1:
+        print(dodawanie())
+        break
+    elif option==2:
+        print(odejmowanie())
+    elif option == 3:
+        print(mnożenie())
+    elif option==4:
+        print(dzielenie())
+    elif option==0:
+        print("Koniec programu")
+        exit
+
