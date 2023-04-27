d2ae2b575e12d41e7ac23a588e30798ff5816d82
Kjeni <kacper.blazejewski88@gmail.com>
2023-04-27 15:18:16 +0200

    zad3

--- 2. Struktury danych/zad3.py
+++ 2. Struktury danych/zad3.py
@@ -1,2 +1,5 @@
 list = ["burak", "cukinia","pomidor","cytryna","ananas","papryka","dynia"]
 letter= input("Wybierz swoją literkę: ")
+for element in list:
+    if element.startswith(letter):
+        print(element, end=" ")
