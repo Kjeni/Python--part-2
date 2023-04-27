1f82c02a7f484b6a56c14e2d9635bf52f01ba29a
Kjeni <kacper.blazejewski88@gmail.com>
2023-04-27 15:15:19 +0200

    zad 2 strukturend

--- 2. Struktury danych/zad2.py
+++ 2. Struktury danych/zad2.py
@@ -1,3 +1,10 @@
 wyraz = input("Podaj słowo: ")
 wyraz = wyraz.lower().replace(" "," ")
+odwroconywyraz = ""
+for index in range(len(wyraz)-1,-1,-1):
+    odwroconywyraz += wyraz[index]
 
+if odwroconywyraz == wyraz:
+    print("Palidrom")
+else:
+    print("Nie jest palidromem")
