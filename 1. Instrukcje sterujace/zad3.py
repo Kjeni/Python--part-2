fcd76cf6e72749000acfaa660adca8e29a602317
Kjeni <kacper.blazejewski88@gmail.com>
2023-04-29 00:50:00 +0200

    uzupelnienie zadan

--- 1. Instrukcje sterujace/zad3.py
+++ 1. Instrukcje sterujace/zad3.py
@@ -1,16 +1,18 @@
 import random
 liczba = random.randint(0,101)
-print(liczba)
+# print(liczba)
 liczbagracza = int(input("Zgadnij jaką liczę mam na myśli: "))
+count = 0
 while True:
-    if liczba < liczbagracza:
+    count += 1
+    if liczba > liczbagracza:
         print("Twoja liczba jest mniejsza od mojej!")
         liczbagracza= int(input("Zgadnij jaką liczę mam na myśli: "))
-    elif liczba > liczbagracza:
+    elif liczba < liczbagracza:
         print("Twoja liczba jest większa od mojej!")
         liczbagracza= int(input("Zgadnij jaką liczę mam na myśli: "))
     else:
-        print(f"Brawo udało ci się w x strzałach")
+        print(f"Brawo udało ci się w {count} strzałach")
         break
 
 
--- 3. Operatory logiczne/zad3.py
+++ 3. Operatory logiczne/zad3.py
@@ -1,10 +1,9 @@
-wiek = int(input("Ile masz lat: "))
-a2 = True
-    if int(input("Czy masz prawo jazdy kat A2(t/n): ")):
-        in ("t","ta","tak","T")
-    else:
-         False
+wiek = int(input("Podaj swój wiek: "))
+category_A2 = int(input("Ile lat posiadasz prawo jazdy kategorii A2? (Jeśli nie posiadasz wpisz 0): "))
 
-if wiek > 24:
-    print("Możesz przystąpic do egzaminu na kat A")
+if wiek >= 24:
+    print("Możesz przystąpić do egzaminu na prawo jazdy kategorii A.")
+elif category_A2 >= 2 and wiek >= 20:
+    print("Możesz przystąpić do egzaminu na prawo jazdy kategorii A.")
 else:
+    print("Nie spełniasz warunków do przystąpienia do egzaminu na prawo jazdy kategorii B.")
