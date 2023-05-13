d32818b31d515ca95d8ac9c8306c854311fa32bb
Kjeni <kacper.blazejewski88@gmail.com>
2023-05-13 22:15:25 +0200

    11

--- 11.Wyjatki/kalk.py
+++ 11.Wyjatki/kalk.py
@@ -4,12 +4,16 @@ def dodawanie():
     wynik = liczba1+liczba2
     informacja = f"{liczba1} + {liczba2} = {wynik}"
     return f"Dodaję: {informacja} ", f"Wynik: {wynik}"
+
 def odejmowanie():
+
     liczba1 = float(input("Podaj pierwszą liczbę: "))
     liczba2 = float(input("Podaj drugą liczbę: "))
     wynik = liczba1-liczba2
     informacja = f"{liczba1} - {liczba2} = {wynik}"
     return f"Odejmuję: {informacja} ", f"Wynik: {wynik}"
+
+        
 def mnożenie():
     liczba1 = float(input("Podaj pierwszą liczbę: "))
     liczba2 = float(input("Podaj drugą liczbę: "))
@@ -22,6 +26,8 @@ def dzielenie():
     wynik = liczba1/liczba2
     informacja = f"{liczba1} : {liczba2} = {wynik}"
     return f"Dzielę: {informacja} ", f"Wynik: {wynik}"
+    
+
 def menu():
     print("[1] Dodawanie")
     print("[2] Odejmowanie")
@@ -29,20 +35,31 @@ def menu():
     print("[4] Dzielenie")
 
 menu()
-option = int(input("Co chcesz zrobić[1-4], aby zakończyć działanie programu [0]: "))
-while option != 0:
-    if option ==1:
-        print(dodawanie())
-        option = int(input("Co chcesz zrobić[1-4]: "))
-    elif option==2:
-        print(odejmowanie())
-        option = int(input("Co chcesz zrobić[1-4]: "))
-    elif option == 3:
-        print(mnożenie())
-        option = int(input("Co chcesz zrobić[1-4]: "))
-    elif option==4:
-        print(dzielenie())
-        option = int(input("Co chcesz zrobić[1-4]: "))
-    elif option==0:
-        break
-print("Koniec programu")
+try:
+    option = int(input("Co chcesz zrobić[1-4], aby zakończyć działanie programu [0]: "))
+
+    while option != 0:   
+        try:
+            if option ==1:
+                print(dodawanie())
+                option = int(input("Co chcesz zrobić[1-4]: "))
+            elif option==2:
+                print(odejmowanie())
+                option = int(input("Co chcesz zrobić[1-4]: "))
+            elif option == 3:
+                print(mnożenie())
+                option = int(input("Co chcesz zrobić[1-4]: "))
+            elif option==4:
+                print(dzielenie())
+                option = int(input("Co chcesz zrobić[1-4]: "))
+            elif option==0:
+                break
+        except ValueError:
+            print("Wprowadzono nieprawidłowe dane. Proszę wprowadzić liczby!")
+        except ZeroDivisionError:
+            print("Nie można dzielić przez zero!")
+    print("Koniec programu")
+except ValueError:
+    print("Następnym razem wybierz jedną z opcji: [1], [2], [3], [4], [0]. Program zakończył swoje działanie.")
+
+
