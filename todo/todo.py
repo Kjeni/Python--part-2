26bd1e3440bc0648e18b3a96ae2ce7b41b8fdb51
Kjeni <kacper.blazejewski88@gmail.com>
2023-05-17 23:47:11 +0200

    funkcje działają poprawnie dodano testowe zapisy oraz funkcje autozapisu

--- todo/todo.py
+++ todo/todo.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime, timedelta
+import os
 
 class Task:
     def __init__(self, task_id, title, description, due_date):
@@ -11,13 +12,13 @@ class Task:
 class TaskManager:
     def __init__(self):
         self.tasks = []
-
+#funkcja dodaj zadanie by id:
     def add_task(self, title, description, due_date):
         task_id = len(self.tasks) + 1
         task = Task(task_id, title, description, due_date)
         self.tasks.append(task)
         print("Dodano nowe zadanie o ID: ", task_id)
-
+#funkcja usuwajaca zadanie by id:
     def delete_task(self, task_id):
         for task in self.tasks:
             if task.task_id == task_id:
@@ -25,7 +26,7 @@ class TaskManager:
                 print("Usunięto zadanie o ID:", task_id)
                 return
         print("Nie znaleziono zadania o podanym ID.")
-
+#funkcja edytujaca zadanie:
     def update_task(self, task_id, title, description, due_date):
         for task in self.tasks:
             if task.task_id == task_id:
@@ -35,13 +36,13 @@ class TaskManager:
                 print("Zaktualizowano zadanie o ID:", task_id)
                 return
         print("Nie znaleziono zadania o podanym ID.")
-
+#funkcja pobierz zadanie by id:
     def get_task(self, task_id):
         for task in self.tasks:
             if task.task_id == task_id:
                 return task
         return None
-
+#funkcja pobierajaca wszystkie zadania:
     def get_all_tasks(self):
         if len(self.tasks) == 0:
             print("Brak zapisanych zadań.")
@@ -50,9 +51,9 @@ class TaskManager:
                 print("ID:", task.task_id)
                 print("Tytuł:", task.title)
                 print("Termin wykonania:", task.due_date.strftime("%Y-%m-%d"))
-                print()
-
-    def save_tasks_to_file(self, filename):
+                print("Opis:", task.description)
+#zapis do pliku:
+    def save_tasks_to_file(self, file_path):
         tasks_data = []
         for task in self.tasks:
             task_data = {
@@ -63,14 +64,14 @@ class TaskManager:
             }
             tasks_data.append(task_data)
 
-        with open(filename, "w") as file:
+        with open(file_path, "w") as file:
             json.dump(tasks_data, file, indent=4)
-
-    def load_tasks_from_file(self, filename):
+#funkcja wczytania z pliku z sortowaniem:(json)
+    def load_tasks_from_file(self, file_path):
         try:
-            with open(filename, "r") as file:
+            with open(file_path, "r") as file:
                 tasks_data = json.load(file)
-
+                task_data = sorted(file, key=lambda x: x['task_id'])
             for task_data in tasks_data:
                 task_id = task_data["task_id"]
                 title = task_data["title"]
@@ -82,32 +83,34 @@ class TaskManager:
             print("Załadowano zapisane zadania.")
         except FileNotFoundError:
             print("Nie znaleziono pliku z zapisanymi zadaniami.")
-
+#funkcja z menu:
 def display_menu():
     print("======== MENU =======")
     print("1. Dodaj nowe zadanie")
     print("2. Wyświetl zadanie")
+    print("2a. Wyświetl zadanie wraz z opisem")
     print("3. Usuń zadanie")
     print("4. Zaktualizuj zadanie")
     print("5. Zapisz zadania do pliku")
     print("6. Wczytaj zadania z pliku")
     print("7. Wyświetl wszystkie zadania")
     print("8. Wyjście")
-
+#walidacja daty:
 def validate_datetime(date_str):
     try:
         return datetime.strptime(date_str, "%Y-%m-%d").date()
     except ValueError:
         print("Niepoprawny format daty. Wprowadź datę w formacie RRRR-MM-DD.")
         return None
-
+#program i pętla sterująca dla użytkownika:
 def main():
     task_manager = TaskManager()
-
+    file_path = os.path.join(os.path.dirname(__file__), "zapisy", "autozapis" )
+    task_manager.load_tasks_from_file(file_path)
+    task_manager.get_all_tasks()
     while True:
         display_menu()
         choice = input("Wybierz opcję: ")
-
         if choice == "1":
             title = input("Podaj tytuł zadania: ")
             description = input("Podaj opis zadania: ")
@@ -116,6 +119,15 @@ def main():
             if due_date is not None:
                 task_manager.add_task(title, description, due_date)
         elif choice == "2":
+            task_id = int(input("Podaj ID zadania do wyświetlenia: "))
+            task = task_manager.get_task(task_id)
+            if task:
+                print("ID:", task.task_id)
+                print("Tytuł:", task.title)
+                print("Termin wykonania:", task.due_date)
+            else:
+                print("Nie znaleziono zadania o podanym ID.")
+        elif choice == "2a":
             task_id = int(input("Podaj ID zadania do wyświetlenia: "))
             task = task_manager.get_task(task_id)
             if task:
@@ -138,25 +150,23 @@ def main():
                 task_manager.update_task(task_id, title, description, due_date)
         elif choice == "5":
             filename = input("Podaj nazwę pliku do zapisu: ")
-            task_manager.save_tasks_to_file(filename)
+            file_path = os.path.join(os.path.dirname(__file__), "zapisy", filename )
+            task_manager.save_tasks_to_file(file_path)
             print("Zapisano zadania do pliku.")
         elif choice == "6":
             filename = input("Podaj nazwę pliku do wczytania: ")
-            task_manager.load_tasks_from_file(filename)
+            file_path = os.path.join(os.path.dirname(__file__), "zapisy", filename )
+            task_manager.load_tasks_from_file(file_path)
         elif choice == "7":
             task_manager.get_all_tasks()
         elif choice == "8":
-            print("UWAGA! Twoje zadania zostaną utracone, jeśli ich nie zapisałeś do pliku.")
-            print("Niepoprawne potwierdzanie zamknięcia skutkuje kontynuowaniem pracy programu")    
-            confirm = input("Aby potwierdź akcję zakończenia programu wpisz 'tak' lub wciśnij klawisz enter aby kontynuować pracę programu: ")
-            if confirm == "tak":
-                print("Program zakończył swoje działanie.")
-                break
-            else:
-                print("Zapisz swoje zadania do pliku :)")
-                continue
+            file_path = os.path.join(os.path.dirname(__file__), "zapisy", "autozapis" )
+            task_manager.save_tasks_to_file(file_path)   
+            print("Program zakończył swoje działanie.")
+            break
         else:
             print("Nieprawidłowa opcja. Wybierz ponownie.")
 
 if __name__ == "__main__":
     main()
+
